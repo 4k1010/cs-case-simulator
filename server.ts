d7da398f162c36ee03152a6b75d14652c647008d
4k1010 <davidwu50803@gmail.com>
2025-12-22 22:03:12 +0800

    add tsx support

--- server.ts
+++ server.ts
@@ -10,7 +10,6 @@ import Inventory from './src/models/Inventory.ts';
 dotenv.config({ path: '.env.local' });
 
 const app = express();
-const PORT = 5000;
 
 // Middleware
 app.use(cors({
@@ -225,6 +224,8 @@ app.delete('/api/inventory', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(Number(PORT), '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
